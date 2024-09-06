6c8f939e761da7408046c664921214ae35e5ad13
la1ni <marilainarasilva@hotmail.com>
2024-09-06 20:38:43 -0300

    First commit

---
+++ agenda/src/Main.java
@@ -0,0 +1,10 @@
+import controller.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        MenuPrincipal menuPrincipal = new MenuPrincipal();
+        menuPrincipal.selecionaOpcao();
+
+    }
+}
